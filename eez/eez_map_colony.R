4b099197085fbdbc5d66b4da8b97b61463e0402e
James Robinson <jpwrobinson@gmail.com>
2023-04-24 22:18:58 +0100

    eez draft 3

--- eez/eez_map_colony.R
+++ eez/eez_map_colony.R
@@ -5,18 +5,19 @@ source('theme_black.R')
 eez<-st_read('eez/World_EEZ_v11_20191118/eez_v11.shp') # polygons
 
 ## eez by country
-eez_c<-eez %>% as.data.frame() %>% 
+eez_c<-eez %>% as.data.frame() %>%
+    mutate(SOVEREIGN1 = recode(SOVEREIGN1, 'Republic of Mauritius' = 'Mauritius',
+                        'Federal Republic of Somalia' = 'Somalia')) %>% 
 		group_by(SOVEREIGN1) %>% 
         mutate(n_territory = n_distinct(TERRITORY1),
                n_territory = ifelse(SOVEREIGN1=='Kiribati', 1, n_territory)) %>% 
         group_by(SOVEREIGN1, n_territory) %>% 
 		summarise(area = sum(AREA_KM2)) %>% 
 		ungroup() %>% 
-		mutate(area_s = rescale(area, to = c(0,1)),
-		       SOVEREIGN1 = recode(SOVEREIGN1, 'Republic of Mauritius' = 'Mauritius',
-		                                        'Federal Republic of Somalia' = 'Somalia'))
+		mutate(area_s = rescale(area, to = c(0,1)))
 
 eez_t<-eez %>% as.data.frame() %>% 
+        mutate(SOVEREIGN1 = recode(SOVEREIGN1, 'Republic of Mauritius' = 'Mauritius')) %>% 
 		group_by(SOVEREIGN1, TERRITORY1) %>% 
 		summarise(area = sum(AREA_KM2)) %>% 
 		ungroup() %>% 
@@ -33,8 +34,7 @@ eez_t<-eez %>% as.data.frame() %>%
                         'French Polynesia' = 'French\nPolynesia',
                         'Marshall Islands' = 'Marshall\nIslands',
                         'Cape Verde' = 'Cape\nVerde',
-                        'Marshall Islands' = "Mashall\nIslands"),
-		       SOVEREIGN1 = recode(SOVEREIGN1, 'Republic of Mauritius' = 'Mauritius')) %>% 
+                        'Marshall Islands' = "Mashall\nIslands")) %>% 
     arrange(terri)
 
 eez_terri_a<-eez_t %>% group_by(terri, SOVEREIGN1) %>% 
@@ -81,29 +81,56 @@ g3<-ggplot(data = dat) +
 # separately but scaled
 
 # country level
-packing1 <- circleProgressiveLayout(eez_c$area) 
+packing1 <- circleProgressiveLayout(c(eez_c$area, max(eez_c$area)))
 dat1 <- circleLayoutVertices(packing1, npoints=50)
 
-packing1$label_eez<-str_replace_all(with(eez_c, SOVEREIGN1), '\\ ', '\n')
-packing1$label_area<-with(eez_c, label_comma()(area))
-packing1$area_prop<-eez_c$area_prop
-packing1$label_area2<-with(eez_c, paste0(round(area_prop,0), '%'))
-packing1$id<-1:nrow(eez_c)
-packing1$n_territory<-eez_c$n_territory
+packing1$label_eez<-c(str_replace_all(with(eez_c, SOVEREIGN1), '\\ ', '\n'), 'maxer')
+packing1$label_area<-c(with(eez_c, label_comma()(area)), 'maxer')
+packing1$area_prop<-c(eez_c$area_prop, 'maxer')
+packing1$label_area2<-c(with(eez_c, paste0(round(area_prop,0), '%')), 'maxer')
+packing1$id<-1:(nrow(eez_c)+1)
+packing1$n_territory<-c(eez_c$n_territory, 'maxer')
 
 ## now territory level, using max country level area as baseline size
-packing2 <- circleProgressiveLayout(c( eez_t$area, max(eez_c$area)))[-252,]
+packing2 <- circleProgressiveLayout(c( eez_t$area, max(eez_c$area)))
 dat2 <- circleLayoutVertices(packing2, npoints=50)
 
-packing2$label_eez<-eez_t$TERRITORY1
+packing2$label_eez<-c(eez_t$TERRITORY1, 'maxer')
 packing2$label_eez2<-str_replace_all(packing2$label_eez, '\\ ', '\n')
 packing2$label_eez2<-str_replace_all(packing2$label_eez2, 'Islands', 'Is.')
 packing2$label_eez2<-str_replace_all(packing2$label_eez2, 'Island', 'Is.')
-packing2$label_eez2<-ifelse(str_length(packing2$label_eez)>20, '', packing2$label_eez2)
-packing2$label_area<-with(eez_t, label_comma()(area))
-packing2$id<-1:nrow(eez_t)
-packing2$terri<-eez_t$terri
+# packing2$label_eez2<-ifelse(str_length(packing2$label_eez)>20, '', packing2$label_eez2)
+packing2$label_area<-c(with(eez_t, label_comma()(area)), 'maxer')
+packing2$id<-1:(nrow(eez_t)+1)
+packing2$terri<-c(eez_t$terri, 'maxer')
 dat2$terri<-packing2$terri[match(dat2$id, packing2$id)]
+
+
+## overseas territory summed level, keeping same total area as packing1 and packing2
+eez_tc<-eez_c %>% filter(!is.na(area_terri)) %>% mutate(SOVEREIGN1 = paste0(SOVEREIGN1, '_O'))
+eez_tc<-rbind(eez_tc, eez_c %>% filter(!is.na(area_terri)) %>% mutate(area_terri = area - area_terri))
+eez_tc<-rbind(eez_tc, eez_c %>% filter(is.na(area_terri)) %>% mutate(area_terri = area))
+packing3 <- circleProgressiveLayout(c(eez_tc$area_terri, max(eez_c$area)))
+dat3 <- circleLayoutVertices(packing3, npoints=50)
+
+packing3$label_eez<-c(eez_tc$SOVEREIGN1, 'maxer')
+packing3$id<-1:(nrow(eez_tc)+1)
+packing3$lab<-c(ifelse(str_detect(eez_tc$SOVEREIGN1, '_O'), 'darkred', '#1e3f5a'), 'maxer')
+dat3$lab<-packing3$lab[match(dat3$id, packing3$id)]
+
+## combine all data together for plotting with fixed sizes
+datter<-rbind(
+    dat1 %>% mutate(idd='one') %>% mutate(terri=NA, lab=NA), 
+    dat2 %>% mutate(idd='two') %>% mutate(lab=NA), 
+    dat3 %>% mutate(idd='three') %>% mutate(terri=NA)
+    )
+
+nam<-c("x","y","radius","label_eez","label_eez2",'area_prop', 'lab', "id","terri", 'idd')
+packer<-rbind(
+    packing1 %>% mutate(idd='one') %>% mutate(label_eez2=NA, terri=NA, lab=NA) %>% select(all_of(nam)), 
+    packing2 %>% mutate(idd='two') %>% mutate(lab=NA, area_prop=NA) %>% select(all_of(nam)), 
+    packing3 %>% mutate(idd='three') %>% mutate(terri=NA, area_prop=NA,label_eez2=NA) %>% select(all_of(nam))
+)
 
 g4<-ggplot(data = dat1) + 
   geom_polygon(aes(x, y, group = id), colour = 'transparent', 
@@ -129,11 +156,45 @@ g5<-ggplot(data = dat2) +
   theme_void() + theme(plot.background=element_rect(fill='black', color='black'), panel.background = element_rect(fill = "black", color  =  NA)) 
 
 
+g6<-ggplot(data = dat3) + 
+    geom_polygon(aes(x, y, group = id, fill=lab), colour = 'transparent', alpha = 1, show.legend = FALSE) +
+    geom_text(data = packing3 %>% filter(lab=='darkred') , aes(x, y, label = label_eez), size = 2.2, col='white', lineheight = .8) +
+    geom_text(data = packing3 %>% filter(lab!='darkred') , aes(x, y, label = label_eez), size = 1.2, col='white', lineheight = .8) +
+    coord_equal() +
+    scale_fill_identity() +
+    theme_void() + theme(plot.background=element_rect(fill='black', color='black'), panel.background = element_rect(fill = "black", color  =  NA)) 
 
-pdf(file = 'eez/eez_sid_territory.pdf', height=7, width =12)
-plot_grid(g4, g5)
-# g3
+g7<-ggplot(data = datter) + 
+    geom_polygon(aes(x, y, group = id), colour = 'transparent', fill = "#1e3f5a", alpha = 1, show.legend = FALSE) +
+    geom_polygon(data = datter %>% filter(idd=='two' & terri == 'Colony'), aes(x, y, group = id), col = 'transparent', 
+                 fill = "darkred", alpha = 1, show.legend = FALSE) +
+    geom_polygon(data = datter %>% filter(idd=='three' & lab == 'darkred'), aes(x, y, group = id), col = 'transparent', 
+                 fill = "darkred", alpha = 1, show.legend = FALSE) +
+    geom_text(data = packer %>% filter(idd=='two' & terri == 'Colony'), aes(x, y, label = label_eez2), size = 1, col='white', lineheight = .8) +
+    geom_text(data = packer %>% filter(idd=='two' & label_eez == 'Antarctica'), aes(x, y, label = label_eez), size = 2.2, col='grey90', lineheight = .8) +
+    geom_text(data = packer %>% filter(idd=='three' & lab=='darkred') , aes(x, y, label = label_eez), size = 1, col='white', lineheight = .8) +
+    geom_text(data = packer %>% filter(idd=='three' & lab=='#1e3f5a') , aes(x, y, label = label_eez), size = 1, col='white', lineheight = .8) +
+    geom_text(data = packer %>% filter(idd=='one' & area_prop > 10 & radius > 400), aes(x, y, label = label_eez), size = 2.2, col='white', lineheight = .8) +
+    geom_text(data = packer %>% filter(idd=='one' & area_prop < 10 | idd=='one' & is.na(area_prop) & radius > 400), aes(x, y, label = label_eez), size = 1.4, col='white', lineheight = .8) +
+    coord_equal() +
+    facet_wrap(~idd) +
+    theme_void() + theme(plot.background=element_rect(fill='black', color='black'), panel.background = element_rect(fill = "black", color  =  NA)) 
+
+
+
+pdf(file = 'eez/eez_sid_territory.pdf', height=7, width =15)
+# plot_grid(g4, g5, g6, nrow=1)
+# plot_grid(g6, g4)
+g7
 dev.off()
 
+sum(eez_t$area[eez_t$terri == 'Colony']) / sum(eez_t$area[eez_t$terri != 'Colony']) * 100
+sum(eez_t$area[eez_t$terri == 'Colony'])
 
-sum(eez_t$area[eez_t$terri == 'Colony']) / sum(eez_t$area[eez_t$terri != 'Colony']) * 100w
+
+eez_tc %>% filter(SOVEREIGN1 %in% c('United Kingdom', 'United States', 'Denmark', 'Spain', 'Portugal')) %>% 
+    summarise(sum(area_terri))
+
+eez_t %>% filter(SOVEREIGN1 %in% c('United Kingdom', 'United States', 'Denmark', 'Spain', 'Portugal')) %>% 
+    ungroup() %>% 
+    summarise(sum(area))
